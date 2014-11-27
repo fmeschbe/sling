f4435d06e6cd32b0e03dd40e1fe163c6b8312253
Felix Meschberger <fmeschbe@adobe.com>
2014-11-27 11:04:08 +0100

    Move Use interface to a new pojo package indicating this is an interface that can be implemented by any Java Pojo which is instantiated as part of processing data-sly-use attributes.

--- contrib/scripting/sightly/engine/src/main/java/org/apache/sling/scripting/sightly/pojo/Use.java
+++ contrib/scripting/sightly/engine/src/main/java/org/apache/sling/scripting/sightly/pojo/Use.java
@@ -16,15 +16,35 @@
  * specific language governing permissions and limitations
  * under the License.
  ******************************************************************************/
-package org.apache.sling.scripting.sightly.use;
+package org.apache.sling.scripting.sightly.pojo;
 
 import javax.script.Bindings;
 
 import aQute.bnd.annotation.ConsumerType;
 
+/**
+ * The <code>Use</code> interface can be implemented by Java objects
+ * which are instantiated as part of processing {@code data-sly-use}
+ * attributes.
+ *
+ * @see <a href="http://docs.adobe.com/docs/en/aem/6-0/develop/sightly.html#use">Sightly Block Statements - Use</a>
+ */
 @ConsumerType
 public interface Use {
 
+    /**
+     * Called to initialize the Java object with the current Java Scripting
+     * API bindings.
+     * <p>
+     * This method is called only if the object has been instantiated by
+     * Sightly as part of processing the {@code data-sly-use} attribute.
+     * <p>
+     * The Java Scripting API bindings provide all the global variables
+     * known to a script being executed. Consider these bindings of a map
+     * from variable name to the variable's value.
+     *
+     * @param bindings The Java Scripting API bindings.
+     */
     public void init(Bindings bindings);
 
 }
